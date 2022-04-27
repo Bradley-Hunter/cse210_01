908e1e3038023ad16672c3b6044594c3ef51e672
Bradley Hunter <hun20053@byui.edu>
2022-04-27 14:37:21 -0600

    Finished checking of win status starting code

--- Program.cs
+++ Program.cs
@@ -19,18 +19,46 @@ namespace TicTacToe
             drawBoard(board);
 
             char playerTurn = 'x';
-            Console.Write($"{playerTurn}'s turn to choose a square (1-9): ");
-            string choice = Console.ReadLine();
-            int square = int.Parse(choice);      
-            if (board[square - 1] == Convert.ToChar(choice))
+            bool done = false;
+            while (!done)
             {
-                updateBoard(board, square, playerTurn);
+                Console.Write($"{playerTurn}'s turn to choose a square (1-9): ");
+                string choice = Console.ReadLine();
+                int square = int.Parse(choice);      
+                if ((board[square - 1] == Convert.ToChar(choice)) && square >= 1 && square <= 9)
+                {
+                    updateBoard(board, square, playerTurn);
+                    drawBoard(board);
+                    done = checkWinStatus(board);
+                    // if (!done)
+
+                }
+                else
+                {
+                    Console.WriteLine("Choose a valid square.");
+                }
             }
-            drawBoard(board);
 
             
         }
 
+        static bool checkWinStatus(List<char> board)
+        {
+            bool win = false;
+            if (board[0] == board[1] && board[1] == board[2] || 
+                board[0] == board[3] && board[3] == board[6] || 
+                board[0] == board[4] && board[4] == board[8] ||
+                board[1] == board[4] && board[4] == board[7] ||
+                board[2] == board[5] && board[5] == board[8] ||
+                board[2] == board[4] && board[4] == board[6] ||
+                board[3] == board[4] && board[4] == board[5] ||
+                board[6] == board[7] && board[7] == board[8])
+                {
+                    win = true;
+                }
+
+            return win;
+        }
         static void updateBoard(List<char> board, int userSquare, char playerTurn)
         {
             board[userSquare - 1] = playerTurn;
