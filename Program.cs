7c845530612f14bfb15194aac6b100ce46523a5e
Bradley Hunter <hun20053@byui.edu>
2022-04-27 15:04:12 -0600

    Finished Tic Tac Toe Game

--- Program.cs
+++ Program.cs
@@ -20,9 +20,10 @@ namespace TicTacToe
 
             char playerTurn = 'x';
             bool done = false;
-            while (!done)
+            bool draw = false;
+            while (!done && !draw)
             {
-                Console.Write($"{playerTurn}'s turn to choose a square (1-9): ");
+                Console.Write($"\n{playerTurn}'s turn to choose a square (1-9): ");
                 string choice = Console.ReadLine();
                 int square = int.Parse(choice);      
                 if ((board[square - 1] == Convert.ToChar(choice)) && square >= 1 && square <= 9)
@@ -30,15 +31,36 @@ namespace TicTacToe
                     updateBoard(board, square, playerTurn);
                     drawBoard(board);
                     done = checkWinStatus(board);
-                    // if (!done)
-
+                    if (!done && board[0] != '1' && board[1] != '2' && 
+                        board[2] != '3' && board[3] != '4' && board[4] != '5' && 
+                        board[5] != '6' && board[6] != '7' && board[7] != '8' && 
+                        board[8] != '9')
+                            {
+                                draw = true;
+                            }
+                    else if (!done && playerTurn == 'x')
+                    {
+                        playerTurn = 'o';
+                    }
+                    else if (playerTurn == 'o' && !done)
+                    {
+                        playerTurn = 'x';
+                    }
+                
                 }
                 else
                 {
                     Console.WriteLine("Choose a valid square.");
                 }
             }
-
+            if (!draw)
+            {
+                Console.WriteLine($"\n{playerTurn} Wins!!!\n\n\n");
+            }
+            else if (draw)
+            {
+                Console.WriteLine("\nThere was a draw.\n\n\n");
+            }
             
         }
 
@@ -66,7 +88,7 @@ namespace TicTacToe
 
         static void drawBoard(List<char> board)
         {
-            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}\n" +
+            Console.WriteLine($"\n{board[0]}|{board[1]}|{board[2]}\n" +
                 $"-+-+-\n{board[3]}|{board[4]}|{board[5]}\n" +
                 $"-+-+-\n{board[6]}|{board[7]}|{board[8]}");
         }
