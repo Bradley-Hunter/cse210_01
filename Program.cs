90bc24909261258204a5287794559f9a0567e12d
Bradley Hunter <hun20053@byui.edu>
2022-04-27 10:05:45 -0600

    Started Tic Tac Toe Game

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿/*
+Assignment: Unit 01 Prove: Developer
+Author(s): Bradley Hunter
+*/
 
-namespace MyApp // Note: actual namespace depends on the project name.
+using System;
+using System.Collections.Generic;
+
+namespace TicTacToe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Bill!");
+            List<char> board = new List<char>(){
+                '1', '2', '3', '4', '5', '6', '7', '8', '9'
+            };
+
+            drawBoard(board);
+
+            char playerTurn = 'x';
+            Console.Write($"{playerTurn}'s turn to choose a square (1-9): ");
+            string choice = Console.ReadLine();
+            char square = char.Parse(choice);      
+
+            
         }
+
+        static void drawBoard(List<char> board)
+        {
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}\n" +
+                $"-+-+-\n{board[3]}|{board[4]}|{board[5]}\n" +
+                $"-+-+-\n{board[6]}|{board[7]}|{board[8]}");
+        }
+
+
     }
 }
